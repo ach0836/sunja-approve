fe7aca2a7990cd4f5265c322f9c11e2aa66d48b0
acholine_0836 <jbs34_1211@js.hs.kr>
2026-03-19 21:30:37 +0900

    fix: update Kakao URL and support contact information; modify premium member names

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -4,10 +4,10 @@ export const STUDY_PERIOD_OPTIONS = [
   { value: "3", label: "야자 3교시" },
 ]
 
-export const DEFAULT_KAKAO_URL = "https://open.kakao.com/o/s66ruUkh"
-export const SUPPORT_CONTACT = "010-8602-9329"
+export const DEFAULT_KAKAO_URL = "https://open.kakao.com/o/sv24femi"
+export const SUPPORT_CONTACT = "010-3870-2570"
 
-export const PREMIUM_MEMBER_NAMES = ["안채헌", "윤석영", "정준호", "박도현"]
+export const PREMIUM_MEMBER_NAMES = ["안채헌", "정승환"]
 
 export const DEFAULT_APPLICANT_OPTIONS = [2, 3, 4, 5]
 export const EXTENDED_APPLICANT_OPTIONS = Array.from({ length: 20 }, (_, i) => i + 1)
